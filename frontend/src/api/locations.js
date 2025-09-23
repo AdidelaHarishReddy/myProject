37e8b5bb5957367b57bd552093d5ace0679a732d
AdidelaHarishReddy <adidelaharish201@gmail.com>
2025-09-23 22:49:42 +0530

    added entrypoint_env.js_changes done in api-url as windows._env_. behalf of process.env.

--- frontend/src/api/locations.js
+++ frontend/src/api/locations.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/locations/`;
+const API_URL = `${window._env_.REACT_APP_API_BASE_URL}/api/locations/`;
 
 // Create Axios instance
 const api = axios.create({
